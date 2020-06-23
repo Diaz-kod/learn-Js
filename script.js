cd9235eecbdf6532f4c8f524acc3a01a03eba699
Diaz <64251978+Diaz-kod@users.noreply.github.com>
2020-06-23 15:05:45 +0300

    Completed table for random color

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Создание массивов обьектов
 function createArr(row, cell) {
 	const arr = []
 	for(let i = 0; i < row; i++) {
@@ -15,7 +15,7 @@ function createArr(row, cell) {
 	return arr
 }
 
-
+// Генерирование массива в таблицу и вынос на экран
 function renderTable(parent, arr) {
 	const table = document.createElement('table');
 	table.classList.add('table');
@@ -33,8 +33,9 @@ function renderTable(parent, arr) {
 	parent.appendChild(table)
 }
 
-
+// Проход спиралью по массиву
 function spiralPassage(arr) {
+	// Направления движения ячейки
 	const direct = {
 		left: {i: 0, j: -1}, 
 		right: {i: 0, j: 1},
@@ -46,25 +47,34 @@ function spiralPassage(arr) {
 	let counter = 0;
     //  Текущее направление
 	let currDirect = 'right';
+	// Цикл делаем плоским, далее В условии цикла проверяем есть ли хотя бы один элемент не false
 	while(arr.flat().some(elem => !elem.passed)) {
+		// Обновляем значение текущей ячейки как проверенную
 		arr[i][j].passed = true;
+		// Внутренности ячейки заполняем каким то числом для отображения
 		arr[i][j].el.innerHTML = counter;
+		// Следующий индекс + текущее напрвление 
 		let nextI = i + direct[currDirect].i;
 		let nextJ = j + direct[currDirect].j;
+		// Проверяем следующая ячейка Undefined или нет
 		let nextCell = arr[nextI] && arr[nextI][nextJ];	
+		// Следующая ячейка не граница? И пройдена ли она?
+			arr[i][j].el.style.background = randomColor();
 		if (nextCell !== undefined && nextCell.passed == false) {
+			// Делаем следующую ячейку текущей
 			i = nextI;
 			j = nextJ;
+			// Увеличиваем значение ячейки на 1 для отображения
 			counter++
-		
 		} else {
+			// Иначе меняем направление движения ячейки
 			currDirect = getNextDirection(currDirect);	
 		}
 	}
 }
 
 
-
+// Функция смены направлений
 function getNextDirection(currDirect) {
 	switch(currDirect) {
 		case 'right': return 'down';
@@ -74,6 +84,14 @@ function getNextDirection(currDirect) {
 	}
 }
 
+// Функция подбора рандомного цвета для ячейки
+function randomColor() {
+	let i = Math.floor(Math.random() * 255);
+	let j = Math.floor(Math.random() * 255);
+	let k = Math.floor(Math.random() * 255);
+	let cell = `rgb(${i}, ${j}, ${k})`;
+	return cell
+}
 
 
 
@@ -82,21 +100,9 @@ function getNextDirection(currDirect) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(randomColor())
 
 const table = createArr(10, 10);
-
 
 renderTable(document.body, table);
 

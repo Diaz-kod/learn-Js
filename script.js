d83ab2004af9bba24fab8784acbc569fcae4042f
Diaz <64251978+Diaz-kod@users.noreply.github.com>
2020-06-26 15:22:13 +0300

    create table

---
+++ script.js
@@ -0,0 +1,43 @@
+// Создаём массив данных
+function createArr(width, height) {
+	let arr = [] 
+		for(let i = 0; i < width; i++) {
+			arr[i] = []
+			for(let j = 0; j < height; j++) {
+				arr[i][j] = {
+					i,
+					j,
+					value: 'empty',
+				}
+			}
+		}
+	return arr
+}
+
+
+// Создаем таблицу поля для игры
+function createTable(parent, arr) {
+	const table = document.createElement('table');
+	table.classList.add('table');
+	for(let i = 0; i < arr.length; i++) {
+		const tr = document.createElement('tr');
+		tr.classList.add('tr');
+		for(let j = 0; j < arr.length; j++) {
+			const td = document.createElement('td');
+			td.innerHTML = 0;
+			tr.appendChild(td)
+			td.classList.add('td');		
+		}
+		table.appendChild(tr);
+	}
+	parent.appendChild(table);
+}
+
+
+
+let arr = createArr(5, 5);
+createTable(document.body, arr);
+
+
+
+

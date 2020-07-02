6b854749fd2696a1029ff19fdd71f433cc832763
Diaz <64251978+Diaz-kod@users.noreply.github.com>
2020-07-02 15:59:52 +0300

    Is ready

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-let allCell = Array.from(document.getElementsByClassName('table-calendar_string_emptyCell'));
+let allCell = Array.from(document.getElementsByClassName('table-calendar_cell'));
 
 function clickCell(arr) {
 	for(let i = 0; i < arr.length; i++) {
 		arr[i].addEventListener('click', () => {
-			reversed(arr[i]);
+			arr.forEach(cell => cell.classList.remove('table-calendar_cell--active'));
+			arr[i].classList.add('table-calendar_cell--active');
 		})
 	}
 } 
 
-function reversed(cell) {
-	// console.log(cell.classList)
-	if(cell.classList.contains('table-calendar_string_emptyCell')) {
-		cell.classList.add('table-calendar_string_activeCell');
-	} else if(cell.classList.contains('table-calendar_string_activeCell')) {
-		cell.classList.add('table-calendar_string_emptyCell');
-		cell.classList.remove('table-calendar_string_activeCell')
-		console.log(cell)
-	}
+// function setActiveCell(cell) {
+// 	if(emptyCell.classList.contains('table-calendar_string_emptyCell')) {
+// 		emptyCell.classList.remove('table-calendar_string_emptyCell');
+// 		emptyCell.classList.add('table-calendar_string_activeCell');
+// 	} else {
+// 		cell.classList.('table-calendar_string_activeCell');
+// 		cell.classList.add('table-calendar_string_emptyCell');
+// 	}
+// }
+
+
+function addZero(value) {
+	if(value < 10) {
+		return '0' + value
+	} 
+	return value
 }
 
+function showTime() {
+	const now = new Date();
+	const time = document.getElementById('nowTime');
+	let hours = now.getHours();
+	let minutes = now.getMinutes(); 
+	let seconds = now.getSeconds();
+	return time.innerHTML = addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds);
+}
 
+setInterval(showTime, 1000)
+
+
+
+
+showTime()
 clickCell(allCell);
